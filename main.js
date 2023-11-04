d374d3d0d572f8d20e826d482fc3acc542d05298
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2023-11-04 14:32:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,3 @@
-function downloadFile(url, fileName){
-  fetch(url, { method: 'get', mode: 'no-cors', referrerPolicy: 'no-referrer' })
-    .then(res => res.blob())
-    .then(res => {
-      const aElement = document.createElement('a');
-      aElement.setAttribute('download', fileName);
-      const href = URL.createObjectURL(res);
-      aElement.href = href;
-      // aElement.setAttribute('href', href);
-      aElement.setAttribute('target', '_blank');
-      aElement.click();
-      URL.revokeObjectURL(href);
-    });
+function Download(url) {
+    document.getElementById('my_iframe').src = url;
 };
-document.querySelector('button').onclick =function () {
-  downloadFile('https://github.com/soulfire-singularity/ethan-n-resume/blob/main/d-20-roller.py', 'd-20-roller.py');
-}
