d5f689afcdecda4e307ee422c60d40003aafbdab
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2023-11-04 14:09:59 -0500

    Update d-20-roller.py

--- d-20-roller.py
+++ d-20-roller.py
@@ -1,1 +1,10 @@
+import random
 
+roll = random.randint(1,20)
+
+print("You rolled a(n) " + str(roll))
+
+if roll == 1:
+    print("You failed")
+elif roll > 1:
+    print("Attack for " + str(roll))
