c394840420a947b90d7c57b53db68536cbfd06d0
Ethan <81447958+soulfire-singularity@users.noreply.github.com>
2023-11-04 14:40:01 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 function Download(url) {
-    document.getElementById('d-20-file').src = url;
+    document.getElementById('d-20-file').src = "https://github.com/soulfire-singularity/ethan-n-resume/blob/main/d-20-roller.py";
 };
